370eb162ee5d3545ca3fa52ffb020547b4540990
Tait Hoyem <code@tait.tech>
2020-10-30 19:27:16 +0000

    Force auth

--- upload/views.py
+++ upload/views.py
@@ -1,7 +1,8 @@
 from sodapy import Socrata
 from core.models import CalgaryAddress
 from download.models import PDF, Address
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.urls import reverse_lazy
 import json
 from .forms import PDFForm
 from django.forms import formset_factory
@@ -35,44 +36,47 @@ def search_api(request, address):
     return HttpResponse(json.dumps(addrs))
 
 def upload(request):
-    if request.method == 'POST':
-        upload_form = PDFForm(request.POST, request.FILES)
-        files = request.FILES.getlist('files')
-        if upload_form.is_valid():
-            for pdf_file in files:
-                print(pdf_file)
-                filename = pdf_file.name
-                code = filename.split(".")[0].split(" ")[-1]
-                city = filename.split(".")[0].split(" ")[-2]
-                address = ' '.join(filename.split(" ")[:-1])
-                name = 'quote_'+code+'.pdf'
-                possible_match = Address.objects.filter(address=address)
-                # if address already entered
-                if len(possible_match) > 0:
-                    pdf = PDF.objects.create(
-                        path=name,
-                        code=code,
-                        upload_file=pdf_file,
-                        address=possible_match[0]
-                    )
-                    pdf.save()
-                else: 
-                    # Save new address
-                    addr = Address.objects.create(
-                        address=address,
-                        city=city
-                    )
-                    # code is currently changable per file
-                    pdf = PDF.objects.create(
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            upload_form = PDFForm(request.POST, request.FILES)
+            files = request.FILES.getlist('files')
+            if upload_form.is_valid():
+                for pdf_file in files:
+                    print(pdf_file)
+                    filename = pdf_file.name
+                    code = filename.split(".")[0].split(" ")[-1]
+                    city = filename.split(".")[0].split(" ")[-2]
+                    address = ' '.join(filename.split(" ")[:-1])
+                    name = 'quote_'+code+'.pdf'
+                    possible_match = Address.objects.filter(address=address)
+                    # if address already entered
+                    if len(possible_match) > 0:
+                        pdf = PDF.objects.create(
                             path=name,
                             code=code,
                             upload_file=pdf_file,
-                            address=addr
-                    )
-                    addr.save()
-                    pdf.save()
-            return HttpResponse("File(s) saved") # TODO: "x Files Saved"
-    elif request.method == "GET":
-        return render(request, 'upload/upload.html', {
-            'form': PDFForm()
-        })
+                            address=possible_match[0]
+                        )
+                        pdf.save()
+                    else: 
+                        # Save new address
+                        addr = Address.objects.create(
+                            address=address,
+                            city=city
+                        )
+                        # code is currently changable per file
+                        pdf = PDF.objects.create(
+                                path=name,
+                                code=code,
+                                upload_file=pdf_file,
+                                address=addr
+                        )
+                        addr.save()
+                        pdf.save()
+                return HttpResponse("File(s) saved") # TODO: "x Files Saved"
+        elif request.method == "GET":
+            return render(request, 'upload/upload.html', {
+                'form': PDFForm()
+            })
+    else:
+        return redirect(reverse_lazy('login'))
