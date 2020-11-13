2e3a38db9d641fbd58eb02ade5cffeea33ce438e
Tait Hoyem <code@tait.tech>
2020-11-13 10:38:21 -0700

    Add basic styling

--- download/forms.py
+++ download/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
 class CodeForm(forms.Form):
-    code = forms.CharField(label='Code', max_length=8, required=True)
-    email = forms.EmailField(label='Email', max_length=32, required=True)
+    code = forms.CharField(label='Code', max_length=8, required=True, 
+        widget=forms.TextInput(
+            attrs={'placeholder': '123A56'}
+    ))
+    email = forms.EmailField(label='Email', max_length=32, required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'jane@joe.com'}
+        ))
