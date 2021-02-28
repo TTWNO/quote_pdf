30c915f7897826cb1af9600719a566f7c44a6209
Tait Hoyem <tait@tait.tech>
2021-02-28 14:20:10 -0700

    Update placeholders

--- download/forms.py
+++ download/forms.py
@@ -3,13 +3,13 @@ from django import forms
 class CodeForm(forms.Form):
     address = forms.CharField(label='Address', max_length=120, required=True,
         widget=forms.TextInput(
-            attrs={'placeholder': '123 Main St.'}
+            attrs={'placeholder': 'Enter your address', 'autocomplete': 'off'}
         ))
     email = forms.EmailField(label='Email', max_length=32, required=True,
         widget=forms.TextInput(
-            attrs={'placeholder': 'jane@joe.com'}
+            attrs={'placeholder': 'Enter your email'}
         ))
     code = forms.CharField(label='Code', max_length=8, required=True, 
         widget=forms.TextInput(
-            attrs={'placeholder': '123A56'}
+            attrs={'placeholder': 'Enter your code'}
     ))
