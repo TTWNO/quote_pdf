5c387340589a20466605d67c6a7e1c8aada71cff
Tait Hoyem <tait@tait.tech>
2021-03-04 13:10:37 -0700

    Simplify admin interface

--- download/admin.py
+++ download/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 
 from . import models
 
-# Register your models here.
-@admin.register(models.BCCEmail)
-class BCCEmailAdmin(admin.ModelAdmin):
-    pass
+## Register your models here.
+#@admin.register(models.BCCEmail)
+#class BCCEmailAdmin(admin.ModelAdmin):
+#    pass
+#
+#@admin.register(models.CCEmail)
+#class CCEmailAdmin(admin.ModelAdmin):
+#    pass
+#
+#@admin.register(models.PDF)
+#class PDFAdmin(admin.ModelAdmin):
+#    list_display = ['address', 'upload_date']
 
-@admin.register(models.CCEmail)
-class CCEmailAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(models.PDF)
-class PDFAdmin(admin.ModelAdmin):
-    list_display = ['address', 'upload_date']
+@admin.register(models.Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['address']
+    search_fields = ['address']
